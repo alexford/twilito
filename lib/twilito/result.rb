e3cb4e21f39773b0197338658de2d9c3f51d3e4c
Alex Ford <alex@cratercreative.com>
2019-11-30 07:38:19 -0500

    #4 Fix Result struct in Ruby 2.4 (#6)
    * Set looser ruby version
    
    * Define Result.initialize with keywords
    
    The keyword_init option for Structs is not supported in Ruby 2.4

--- lib/twilito/result.rb
+++ lib/twilito/result.rb
@@ -1,21 +1,25 @@
 # frozen_string_literal: true
 
 module Twilito
-  Result = Struct.new(:success?, :errors, :sid, :response, keyword_init: true) do
+  Result = Struct.new(:success, :errors, :sid, :response) do
+    def initialize(success:, errors: [], sid: nil, response: nil)
+      super(success, errors, sid, response)
+    end
+
     def self.success(**args)
-      new(success?: true, **args)
+      new(success: true, **args)
     end
 
     def self.failure(**args)
-      new(success?: false, **args)
-    end
-
-    def errors
-      to_h[:errors] || []
+      new(success: false, **args)
     end
 
     def data
       JSON.parse(response_body || '{}')
+    end
+
+    def success?
+      to_h[:success] || false
     end
 
     private
